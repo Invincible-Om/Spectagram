fc31062a4fae07e87d98b205e6a15dc1bb0d6cf5
Abhaykrit Singh <76990881+Invincible-Om@users.noreply.github.com>
2022-05-30 15:48:24 +0530

    Add files via upload

---
+++ screens/Feed.js
@@ -0,0 +1,20 @@
+import React,{Component} from "react";
+import { View,Text } from "react-native";
+
+export default class Feed extends Component {
+    render(){
+        return (
+            <View>
+
+
+                <Text>
+
+                    feed
+
+                </Text>
+
+
+            </View>
+        );
+    }
+}
